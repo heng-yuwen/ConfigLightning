d76dd05d642362bb62b27fc17fb1e24fcbe59f86
Yuwen Heng <y.heng@soton.ac.uk>
2022-09-23 17:04:59 +0800

    support none spectral, for test purpose

--- customise_pl/transforms/spectral_recovery_transforms.py
+++ customise_pl/transforms/spectral_recovery_transforms.py
@@ -13,7 +13,8 @@ class SpectralNorm(pl.LightningModule):
     def forward(self, rgb, spectral):
         rgb = (rgb - rgb.min()) / (rgb.max() - rgb.min())
         rgb = np.transpose(rgb, [2, 0, 1])  # [3,482,512]
-        spectral = np.transpose(spectral, [0, 2, 1])
+        if spectral is not None:
+            spectral = np.transpose(spectral, [0, 2, 1])
 
         return rgb, spectral
 
@@ -26,15 +27,18 @@ class SpectralRotateFlip(pl.LightningModule):
     def forward(self, rgb, spectral):
         for j in range(random.randint(0, 3)):
             rgb = np.rot90(rgb.copy(), axes=(1, 2))
-            spectral = np.rot90(spectral.copy(), axes=(1, 2))
+            if spectral is not None:
+                spectral = np.rot90(spectral.copy(), axes=(1, 2))
         # Random vertical Flip
         if random.random() > self.p:
             rgb = rgb[:, :, ::-1].copy()
-            spectral = spectral[:, :, ::-1].copy()
+            if spectral is not None:
+                spectral = spectral[:, :, ::-1].copy()
         # Random horizontal Flip
         if random.random() > self.p:
             rgb = rgb[:, ::-1, :].copy()
-            spectral = spectral[:, ::-1, :].copy()
+            if spectral is not None:
+                spectral = spectral[:, ::-1, :].copy()
 
         return rgb, spectral
 
@@ -48,6 +52,7 @@ class SpectralRandomCrop(pl.LightningModule):
     def forward(self, rgb, spectral):
         i, j, h, w = transforms.RandomCrop.get_params(torch.tensor(rgb), self.patch_size)
         rgb = rgb[:, i:i + h, j: j+w]
-        spectral = spectral[:, i:i + h, j: j+w]
+        if spectral is not None:
+            spectral = spectral[:, i:i + h, j: j+w]
 
         return rgb, spectral
